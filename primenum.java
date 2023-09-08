9a5462ddead5e011f66348c66c59b11bc058ea95
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:22:23 +0530

    Add files via upload

---
+++ primenum.java
@@ -0,0 +1,31 @@
+package com.jsp.primenum;
+
+import java.util.Scanner;
+
+public class primenum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner (System.in);
+		int a = scn.nextInt();
+		int count = 0;
+		for(int i=0; i<=0;i++) {
+			if(a%i==0) {
+				count++;
+			}
+		}
+		if(count==2) {
+			
+		}
+		System.out.println("given numis prime");
+	}
+	else {
+	System.out.println("given number is nor prime num");
+	
+		
+		
+		
+
+	}
+
+}
