ccae49af1cfead5c9ffedcf4b8c285cfe90f8aae
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:00:47 +0530

    Add files via upload

---
+++ Siva.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Siva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn=new Scanner(System.in);
+int n=scn.nextInt();
+System.out.println("enter a num");
+if(n/2==0);
+{
+	System.out.println("even num");
+}
+	
+	}
+
+}
