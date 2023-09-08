93f27ea8e131afde2f98442f8520b6ba929a11ff
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:21:31 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,15 @@
+package com.jsp.operaters;
+
+public class bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 47;
+		System.out.println(a&b);
+		System.out.println(a/b);
+		System.out.println(a<<b);
+
+	}
+
+}
---
+++ grade.java
@@ -0,0 +1,17 @@
+package com.jsp.operaters;
+
+public class grade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int testscore = 80;
+		int grade = 0 ;
+		 if(testscore>=90) {
+		 }else {
+			 grade='f';
+		 }
+		 System.out.println("Grade = " + grade);
+
+	}
+
+}
