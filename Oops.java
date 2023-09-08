b32083ccee91dd770524a14c257dedcb94e31dae
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:01:54 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ class p1
+{
+	public static void main (string []args)
+	Scanner Scan = new scanner (Sysyem.in);
+	System.out.println("enter the number");
+	int size = scanner.nextint();
+	for(int i=0;i<=5;i++);{
+	}
+	for(int j=0;j<=5;j++);{
+	System.out.println(*);
+}
+   system.out.println();
+          
+	
+
+
+	}
