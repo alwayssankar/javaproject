138aacc577003d413a0916cd20c9c952ee2dd349
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:18:50 +0530

    Add files via upload

---
+++ linkedhashset.java
@@ -0,0 +1,21 @@
+package com.jsp.linkedhashset;
+import java.util.linkedhashset;
+
+public class linkedhashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		linkedhashset lset = new linkedhashset();
+		lset.add(100);
+		lset.add(500);
+		lset.add(1200);
+		lset.add(1400);
+		lset.add(1600);
+		System.out.println(lset);
+		
+		
+		
+
+	}
+
+}
