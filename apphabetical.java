195bf6dcd30949a4cb09fc824c7de4784616ccb4
Chilamathuru Sankar <131135557+alwayssankar@users.noreply.github.com>
2023-09-08 14:12:27 +0530

    Add files via upload

---
+++ apphabetical.java
@@ -0,0 +1,25 @@
+package com.jsp.alphabetical;
+
+public class apphabetical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           String str = "NARAYANA";
+           char [] arr = str.toCharArray();
+           char temp;
+           for(int i=0; i<=arr.length; i++) {
+        	   for(int j=i+1; j<=arr.length; j++) {
+        		   if(arr[i]>arr[j]) {
+        			   temp = arr[i];
+        			   arr[i]=arr[j];
+        			   arr[j]=temp;
+        			   
+        		   }
+        	   }
+           }
+           System.out.println(new String(arr));
+           
+
+	}
+
+}
